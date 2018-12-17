b4229a1fdc8b7cb59e002ecd199f045f45405673
Jamal A. Williams <jamalw@della5.princeton.edu>
2018-12-17 16:14:19 -0500

    remove motion artifacts now and also removing z-scoring

--- searchlight_wva_human_bounds_all_windows.py
+++ searchlight_wva_human_bounds_all_windows.py
@@ -8,6 +8,7 @@ import nibabel as nib
 import os
 from scipy.spatial import distance
 import matplotlib.pyplot as plt
+from sklearn import linear_model
 
 subjs = ['MES_022817_0','MES_030217_0','MES_032117_1','MES_040217_0','MES_041117_0','MES_041217_0','MES_041317_0','MES_041417_0','MES_041517_0','MES_042017_0','MES_042317_0','MES_042717_0','MES_050317_0','MES_051317_0','MES_051917_0','MES_052017_0','MES_052017_1','MES_052317_0','MES_052517_0','MES_052617_0','MES_052817_0','MES_052817_1','MES_053117_0','MES_060117_0','MES_060117_1']
 
@@ -72,10 +73,19 @@ def searchlight(coords,human_bounds,mask,song_idx,song_bounds,subjs,hrf,srm_k):
                data = []
                for i in range(len(subjs)):
                    subj_data = np.load(datadir + subjs[i] + '/' + str(x) + '_' + str(y) + '_' + str(z) + '.npy')
-                   data.append(np.nan_to_num(stats.zscore(subj_data[:,:,0],axis=1,ddof=1)))
+                   subj_regs = np.genfromtxt(datadir + subjs[i] + '/EPI_mcf1.par')
+                   motion = subj_regs.T
+                   regr = linear_model.LinearRegression()
+                   regr.fit(motion[:,0:2511].T,subj_data[:,:,0].T)
+                   subj_data1 = subj_data[:,:,0] - np.dot(regr.coef_, motion[:,0:2511]) - regr.intercept_[:, np.newaxis]
+                   data.append(subj_data1)
                for i in range(len(subjs)):
-                   subj_data = np.load(datadir + subjs[i] + '/' + str(x) + '_' + str(y) + '_' + str(z) + '.npy')
-                   data.append(np.nan_to_num(stats.zscore(subj_data[:,:,1],axis=1,ddof=1))) 
+                   subj_regs = np.genfromtxt(datadir + subjs[i] + '/EPI_mcf2.par')
+                   motion = subj_regs.T
+                   regr = linear_model.LinearRegression()
+                   regr.fit(motion[:,0:2511].T,subj_data[:,:,1].T)
+                   subj_data2 = subj_data[:,:,1] - np.dot(regr.coef_, motion[:,0:2511]) - regr.intercept_[:, np.newaxis]
+                   data.append(subj_data2) 
                print("Running Searchlight")
                # only run function on searchlights with #of voxels greater than or equal to min_vox
                if data[0].shape[0] >= min_vox:
